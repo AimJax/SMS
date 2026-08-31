4f499da10d072e009ef1c28d0051235f6df24689
AimJax <rizkyviriano024@gmail.com>
2026-09-01 00:31:23 +0700

    Updated project files

---
+++ Github Push.sh
@@ -0,0 +1,8 @@
+# 1. Stage all new and modified files
+git add .
+
+# 2. Commit your changes with a descriptive message
+git commit -m "Updated project files"
+
+# 3. Push the updates to GitHub
+git push
