81b26dbc115fdbb619ad3287c8839f05b45b7bea
AimJax <rizkyviriano024@gmail.com>
2026-09-01 20:36:50 +0700

    Update Android config to use network IP, suppress CS8602 warnings

--- Client/Configuration/AppConfig.cs
+++ Client/Configuration/AppConfig.cs
@@ -9,7 +9,7 @@ public class AppConfig
     /// Base URL of the API server.
     /// For Android emulator use: http://10.0.2.2:5225 (emulator's host loopback)
     /// For iOS simulator use: http://localhost:5225
-    /// For physical device: Use the development machine's local network IP address.
+    /// For physical device on same network: Use the development machine's local network IP address.
     /// </summary>
-    public string ApiBaseUrl { get; set; } = "http://10.0.2.2:5225";
+    public string ApiBaseUrl { get; set; } = "http://192.168.1.47:5225";
 }
